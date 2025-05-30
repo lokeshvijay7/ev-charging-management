3760b6c7c986303474ffc50f2d484fd69622cbc4
lokeshvijay7 <lokeshlokesh93661@gmail.com>
2025-05-30 23:51:59 +0530

    Connect mongo DB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import connectDB from './config/db.js'; 
+dotenv.config();
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -9,8 +12,9 @@ app.get('/', (req, res) => {
 }
 );
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server is running on http://localhost:${port}`);
 }
 );
 export default app;
